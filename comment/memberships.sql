fcffe26d709447caf6752e9b9b2dfc8ae0c08406
CurvedMirror <1287135821@qq.com>
2019-06-03 09:30:17 +0800

    添加了sql脚本

---
+++ comment/memberships.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.28 : Database - membershipms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`membershipms` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `membershipms`;
+
+/*Table structure for table `user_type` */
+
+DROP TABLE IF EXISTS `user_type`;
+
+CREATE TABLE `user_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_type` */
+
+insert  into `user_type`(`id`,`name`) values (1,'普通会员'),(2,'超级会员'),(3,'年费会员'),(4,'管理员');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `last_modify_time` date DEFAULT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`name`,`password`,`status`,`last_modify_time`,`type_id`) values (1,'jie123','123123',1,'2019-05-10',1),(2,'大佬','123123',0,'2019-05-18',2),(3,'ryj','123',1,'2019-05-11',3);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
